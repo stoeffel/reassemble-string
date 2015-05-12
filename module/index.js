4bd223898a6acc39ba409ff7899e43e264ac158e
Christoph Hermann <christoph.hermann@kaba.com>
2015-05-12 22:46:21 +0200

    fix some edge cases

--- module/index.js
+++ module/index.js
@@ -1,12 +1,13 @@
 const REGEX = /(\w[_\-\s\.]+\w|[a-z][A-Z])/g;
 
+const clean = str => str.replace(/(^[_\-\s\.]+|[_\-\s\.]+$)/, '');
 const firstChar = (str) => str.slice(0, 1);
 const lastChar = (str) => str.slice(-1);
 
-const reassembleString = (combine, str) => str.replace(REGEX, sub => combine(firstChar(sub), lastChar(sub)));
+const reassembleString = (combine, str) => clean(str).replace(REGEX, sub => combine(firstChar(sub), lastChar(sub)));
 
-export default (combine, str) => 
-	str ? 
+export default (combine, str) =>
+	str ?
 		reassembleString(combine, str):
 		(str) => reassembleString(combine, str);
 
--- tests/test.js
+++ tests/test.js
@@ -5,8 +5,14 @@ test('#reassembleString', () => {
   const camelize = reassembleString((a, b) => a + b.toUpperCase());
   const dasherize = reassembleString((a, b) => `${a}-${b.toLowerCase()}`);
 
+  equal(camelize(''), '');
+  equal(camelize('a'), 'a');
+  equal(camelize('a a'), 'aA');
   equal(camelize('hello-world'), 'helloWorld');
   equal(camelize('hello_world'), 'helloWorld');
+  equal(camelize('hello_'), 'hello');
+  equal(camelize('_world'), 'world');
+  equal(camelize('-_world'), 'world');
   equal(camelize('hello - world'), 'helloWorld');
   equal(camelize('hello.world'), 'helloWorld');
   equal(camelize('hello + world'), 'hello + world');
